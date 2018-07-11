b6c370a9f8dab9dc342b27e38f9d0ff8a1eb902b
Douglas Wachtel <douglas.wachtel@gmail.com>
2018-07-11 15:29:30 -0400

    FizzBuzz

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Execute();
+            Console.ReadLine();
+        }
+
+        static void Execute()
+        {
+            for(int i = 1; i <= 100; i++)
+            {
+                Console.Write(i);
+                if(i % 3 == 0)
+                {
+                    Console.Write("Fizz");
+                }
+                if(i % 5 ==0)
+                {
+                    Console.Write("Buzz");
+                }
+                Console.Write("\n");
+            }
+        }
+    }
+}
